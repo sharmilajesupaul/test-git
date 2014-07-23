406cc7c1e6caab390dab8c1eef114616b236d37a
MysticMania <MysticMania@users.noreply.github.com>
2014-07-23 15:40:13 -0700

    Update words.rb

--- words.rb
+++ words.rb
@@ -1,3 +1,5 @@
+#This program instructs the user to insert one word on each line until the enter key is used two consecutive times.
+#Then the words are sorted and displayed in the terminal. 
 puts "Please enter one word on each line:"
 words = []
 
